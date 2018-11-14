17cd3d5a7cf8c1dfd062aa8aeadad93b3da833c3
Ariel Levi Simons <levisimons@gmail.com>
2018-11-13 18:30:30 -0800

    Update CALeDNAZetaFactors.R

--- CALeDNAZetaFactors.R
+++ CALeDNAZetaFactors.R
@@ -1,65 +1,53 @@
-# /usr/usc/R/3.4.4/bin/Rscript to run on the USC cluster
 require(zetadiv)
 require(plyr)
-require(ggmap)
-require(maps)
-require(mapview)
-require(mapdata)
-require(munsell)
-require(leaflet)
-require(devtools)
-require(webshot)
 
 setwd("~/Desktop/CALeDNA") #Change on the cluster to ~/panfs/CALeDNA
+
+fileList <- list.files(pattern="asv_deco_dedup") #Get all of the metagenomic data tables to calculate zeta_4 diversity for given areas.
+file <- fileList[1]
 
 metadata <- read.table("Final_metadata.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE) #CALeDNA site metadata.
 #Add in the number of samples per location in the metadata.
 locationFrequency <- as.data.frame(table(metadata$loc))
 colnames(locationFrequency) <- c("loc","Freq")
 metadata <- join(metadata,locationFrequency,by=c("loc"))
-names(metadata)[names(metadata) == "loc"] <- "location"
 
-#If the file CALeDNAZetaDecay.txt was already generated on the cluster using Zeta4eDNA.R read it in here.
-zetaLocation <- read.table("CALeDNAZetaDecay.txt", header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
+#Read in OTU table.
+DataRaw <- read.table(file, header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
+#Get primer name.
+primer <- strsplit(file,"_")
+primer <- primer[[1]][length(primer[[1]])]
+primer <- strsplit(primer,".csv")
+primer <- primer[[1]][1]
+#Get OTU names
+taxa <- DataRaw[,1]
+#Transpose the OTU table and merge it with the sample metadata
+DataT <- as.data.frame(t(DataRaw[,-c(1)]))
+colnames(DataT) <- taxa
+DataT$MatchName <- rownames(DataT)
+MergedData <-join(DataT,metadata,by=c("MatchName"))
+#Filter out areas with less than a certain number of a samples.
+zetaNum=4
+data.spec <- subset(MergedData,Freq>=zetaNum)
+data.spec <- data.spec[,colnames(data.spec) %in% taxa]
+#Create sample location data frame.
+data.xy <- subset(MergedData,Freq>=zetaNum)
+data.xy <- data.xy[,c("Latitude","Longitude")]
+#Create sample factor data frame.
+data.env <- subset(MergedData,Freq>=zetaNum)
+data.env <- data.env[,c("elev","Slope","greenness","hii","loc","phihox","orcdrc","CTI")]
+data.env$hii <- as.numeric(data.env$hii)
+data.env$phihox <- as.numeric(data.env$phihox)
+data.env$orcdrc <- as.numeric(data.env$orcdrc)
+data.env$loc <- as.factor(data.env$loc)
 
-#Choose a primer to subset zeta diversity results.
-primerList <- unique(zetaLocation$primer)
-primer <- "18S"
-zetaPrimerSubset <- zetaLocation[which(zetaLocation$primer==primer),]
-#Merge zeta diversity data with sample site metadata.
-zetaAnalysis <- join(zetaPrimerSubset,metadata,by=c("location"))
+zetaFactors <- Zeta.msgdm(data.spec=data.spec,data.env=data.env,xy=data.xy,order=4,sam=300,reg.type="glm",distance.type="ortho",normalize=FALSE,rescale=FALSE)
 
-#To generate map of data for a given zeta diversity parameter in California.
-dev.off()
-MapCoordinates <- zetaAnalysis
-#Map data.
-CalMap = leaflet(MapCoordinates) %>% 
-  addTiles()
-ColorScale <- colorNumeric(palette=rainbow(10),domain=MapCoordinates$PLExp)
-CalMap %>% addCircleMarkers(color = ~ColorScale(PLExp), fill = TRUE,radius=0.1,fillOpacity = 0.1) %>% 
-  addProviderTiles(providers$Esri.WorldTopoMap) %>%
-  addLegend("topright", pal=ColorScale,values=~PLExp,title=paste(primer,"Power-law exponent"))
+#Zeta.varpart returns a data frame with one column containing the variation explained by each component 
+#a (the variation explained by distance alone),
+#b (the variation explained by either distance or the environment),
+#c (the variation explained by the environment alone) and 
+#d (the unexplained variation).
+zetaVar <- Zeta.varpart(zetaFactors)
 
-#Comparison of zeta diversity parameters between communities derived using different primer sets.
-zetaPrimerComparison <- data.frame()
-i=0
-for(primer in primerList){
-  zetaPrimerSubset <- zetaLocation[which(zetaLocation$primer==primer),]
-  zetaPrimerSubset <- zetaPrimerSubset[,c("location","zeta4","PLExp")]
-  zetaPrimerSubset <- zetaPrimerSubset[order(zetaPrimerSubset$location),]
-  colnames(zetaPrimerSubset) <- c("location",paste(primer,"zeta4",sep=""),paste(primer,"PLExp",sep=""))
-  i=i+1
-  if(i==1){
-    zetaPrimerComparison <- zetaPrimerSubset
-  } else{
-    zetaPrimerComparison <- join(zetaPrimerComparison,zetaPrimerSubset,by=c("location"))
-  }
-}
-
-#Regression between network parameters.
-require(Hmisc)
-require(corrplot)
-require("PerformanceAnalytics")
-#Each significance level is associated to a symbol : p-values(0, 0.001, 0.01, 0.05, 0.1, 1) <=> symbols(“***”, “**”, “*”, “.”, " “)
-chart.Correlation(zetaPrimerComparison[,c("16Szeta4","18Szeta4","CO1zeta4","FITSzeta4","PITSzeta4")], histogram=FALSE, method="spearman")
-chart.Correlation(zetaPrimerComparison[,c("16SPLExp","18SPLExp","CO1PLExp","FITSPLExp","PITSPLExp")], histogram=FALSE, method="spearman")
+zetaDistance <- Zeta.ddecay(xy=data.xy,data.spec=data.spec,order=2,sam=100,distance.type="ortho",rescale=FALSE,normalize=FALSE,trsf="log10")
