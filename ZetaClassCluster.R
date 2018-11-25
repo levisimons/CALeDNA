d924bfa61a3d261af050d06319402d0f378cb1f9
Ariel Levi Simons <levisimons@gmail.com>
2018-11-25 14:14:27 -0800

    Update ZetaClassCluster.R

--- ZetaClassCluster.R
+++ ZetaClassCluster.R
@@ -5,41 +5,74 @@ require(dplyr)
 
 #This script analyzes zeta diversity patterns for community groups defined by presence/absence data clustered to class level.
 #CALeDNA site metadata.
-setwd("~/Desktop/CALeDNA") #Change on the cluster to ~/panfs/CALeDNA
+wd <- "~/Desktop/CALeDNA" #Change on the cluster to ~/panfs/CALeDNA
+setwd(wd)
 metadata <- read.table("Final_metadata.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
+metadata$clust <- as.factor(as.character(metadata$clust))
+#Get the groups for the type of environmental factors.
+factorGroups <- read.table("Metadata_explanation.csv", header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE)
+#Unique environmental factors are as follows:
+uniqueFactors <- c("Location","Topology","Habitat","BioClim","Soil Properties","Vegetation")
 #Get the cluster ID of all of the sample sites where locations have sufficient numbers of samples.
 clusteredSites <- metadata[,c("MatchName","Zeta_4ID")]
 clusteredSites <- subset(clusteredSites,is.na(clusteredSites$Zeta_4ID)==FALSE)
 
-setwd("~/Desktop/CALeDNA/ASVbyClass") #Change on the cluster to ~/panfs/CALeDNA/ASVbyClass
-
 #Get all of the metagenomic data tables, split by class, within a primer set.
-Files16S <- list.files(pattern="asv_16S")
-Files18S <- list.files(pattern="asv_18S")
-FilesCO1 <- list.files(pattern="asv_CO1")
-FilesPITS <- list.files(pattern="asv_PITS")
-FilesFITS <- list.files(pattern="asv_FITS")
+Files16S <- list.files(path=paste(wd,"/ASVbyClass",sep=""),full.names=TRUE,pattern="asv_16S")
+Files18S <- list.files(path=paste(wd,"/ASVbyClass",sep=""),full.names=TRUE,pattern="asv_18S")
+FilesCO1 <- list.files(path=paste(wd,"/ASVbyClass",sep=""),full.names=TRUE,pattern="asv_CO1")
+FilesPITS <- list.files(path=paste(wd,"/ASVbyClass",sep=""),full.names=TRUE,pattern="asv_PITS")
+FilesFITS <- list.files(path=paste(wd,"/ASVbyClass",sep=""),full.names=TRUE,pattern="asv_FITS")
 FilesAll <- list(Files16S,Files18S,FilesCO1,FilesPITS,FilesFITS)
 
 #Create a presence/absence OTU table, clustered by class, within a primer set.
 OTUClass <- data.frame()
-for(File16S in Files16S){
-  OTURaw <- read.table(File16S, header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE) #Individual OTU table by class.
+#Choose a primer set
+primerFiles <- Files18S
+for(primerFile in primerFiles){
+  OTURaw <- read.table(primerFile, header=TRUE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE) #Individual OTU table by class.
   if(nrow(OTURaw)>1){
     OTURaw <- OTURaw[,-c(1)]
     siteNames <- colnames(OTURaw)
     OTURaw <- sapply(OTURaw,as.numeric)
     OTURow <- as.data.frame(t(as.data.frame(colSums(OTURaw))))
-    className <- strsplit(strsplit(File16S,"_")[[1]][3],".csv")[[1]][1]
+    className <- strsplit(strsplit(primerFile,"_")[[1]][3],".csv")[[1]][1]
     rownames(OTURow) <- className
     colnames(OTURow) <- siteNames
     OTUClass <- rbind(OTUClass,OTURow)
   }
 }
+#Creat OTU tables by primer group, clustered to class.
 OTUClass[OTUClass > 0] <- 1
+OTUClass <- as.data.frame(t(OTUClass))
+
+#Determine the contributions to the variation in zeta diversity due to
+#geographic separation, environmental factor group, or some unknown factor.
+for(uniqueFactor in uniqueFactors){
+  factorSubset <- subset(factorGroups,Category==uniqueFactor | Category=="Coordinates")
+  metadataSubset <- metadata
+  rownames(metadataSubset) <- metadataSubset$MatchName
+  metadataSubset <- subset(metadataSubset,is.na(metadataSubset$Zeta_4ID)==FALSE) #Keep sites with a geographic cluster ID.
+  metadataSubset <- metadataSubset[,colnames(metadataSubset) %in% factorSubset$`Column name` | colnames(metadataSubset) %in% c("Latitude","Longitude")] #Keep factors by group, along with GPS coordinates.
+  metadataSubset <- metadataSubset[complete.cases(metadataSubset),] #Remove rows with missing data.
+  data.xy <- metadataSubset[,c("Latitude","Longitude")] #Create location data frame.
+  metadataSubset <- metadataSubset[,-which(names(metadataSubset) %in% c("Latitude","Longitude")),drop=FALSE] #Remove GPS coordinates from factor group for zeta diversity analysis.
+  #Force metadata factors into numeric or factor types.
+  metadataSubset[sapply(metadataSubset, is.character)] <- lapply(metadataSubset[sapply(metadataSubset, is.character)], as.factor)
+  metadataSubset[sapply(metadataSubset, is.integer)] <- lapply(metadataSubset[sapply(metadataSubset, is.integer)], as.numeric)
+  #Subset the OTU table by the remaining sample sites
+  data.OTU <- OTUClass[which(row.names(OTUClass) %in% row.names(metadataSubset)),]
+  #Determine contibutions of variance to zeta diversity from environmental factors
+  zetaFactor <- Zeta.msgdm(data.spec=data.OTU,data.env=metadataSubset,xy=data.xy,order=4,method.glm="glm.fit.cons",distance.type="ortho")
+  zetaVars <-  Zeta.varpart(zetaFactor,method.glm="glm.fit.cons")
+  print(uniqueFactor)
+  print(zetaVars$`Adjusted Rsq`)
+}
 
 #Create subset presence/absence tables for each geographic cluster.
+#This is useful for downstream analysis such as mapping.
 zetaNum=4 #Highest order of zeta diversity.
+zetaCluster <- data.frame()
 for(clusterID in unique(clusteredSites$Zeta_4ID)){
   localCluster <- subset(clusteredSites,clusteredSites$Zeta_4ID==clusterID)
   localClusterOTUs <- OTUClass[,colnames(OTUClass) %in% localCluster$MatchName]
@@ -55,4 +88,5 @@ for(clusterID in unique(clusteredSites$Zeta_4ID)){
   dat[1,8] <- zetaDecay$zeta.pl$coefficients[2]
   dat[1,9] <- zetaDecay$aic$AIC[2]
   print(dat)
+  zetaCluster <- rbind(zetaCluster,dat)
 }
